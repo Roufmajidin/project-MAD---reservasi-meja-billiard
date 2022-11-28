682a0be7e4b7bab7bdb99522e9e9d908d538e118
Roufmajidin <roufmajidin777@gmail.com>
2022-11-29 04:17:37 +0700

    splash + page All paket

--- lib/app/modules/splash/views/splash_view.dart
+++ lib/app/modules/splash/views/splash_view.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:project_mad/app/modules/home/views/home_view.dart';
+import 'package:splashscreen/splashscreen.dart';
 
 import '../controllers/splash_controller.dart';
 
@@ -8,10 +10,18 @@ class SplashView extends GetView<SplashController> {
   const SplashView({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Container(width: 350, height: 350, color: Colors.red),
+    return SplashScreen(
+      seconds: 3,
+      navigateAfterSeconds: Home(),
+      backgroundColor: Colors.black,
+      image: Image.asset(
+        height: 120,
+        'assets/splash.png',
       ),
+      title: Text("MAD Project"),
+      useLoader: true,
+      loaderColor: Colors.white,
+      loadingText: Text("Loading .."),
     );
   }
 }
