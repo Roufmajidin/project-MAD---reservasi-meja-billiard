67eff7d74e390cf17be6c641f33ce4e19fec4862
rouf_majidin <roufmajidin777@gmail.com>
2023-01-31 00:19:44 +0700

    last -

--- lib/app/modules/prevent_home_admin/views/prevent_home_admin_view.dart
+++ lib/app/modules/prevent_home_admin/views/prevent_home_admin_view.dart
@@ -20,47 +20,63 @@ class PreventHomeAdminView extends GetView<PreventHomeAdminController> {
   Widget build(BuildContext context) {
     return GetBuilder<PreventHomeAdminController>(builder: (controller) {
       return Scaffold(
-          backgroundColor: Color(0xf19191E),
-          body: SafeArea(
-            child: IndexedStack(
-              index: controller.tabIndex,
-              children: [
-                PemesananView(),
-                ReservationEditView(),
-                HomeAdminView()
-              ],
-            ),
+        backgroundColor: Color(0xf19191E),
+        body: SafeArea(
+          child: IndexedStack(
+            index: controller.tabIndex,
+            children: [
+              PemesananView(),
+              ReservationEditView(),
+              HomeAdminView(),
+              HomeAdminView(),
+            ],
           ),
-          bottomNavigationBar: BottomNavigationBar(
-              type: BottomNavigationBarType.fixed,
-              selectedItemColor: Colors.white,
-              backgroundColor: Colors.black,
-              showSelectedLabels: true,
-              showUnselectedLabels: false,
-              currentIndex: controller.tabIndex,
-              onTap: (index) {
-                controller.changeTabIndex(index);
-              },
-              items: [
-                BottomNavigationBarItem(
-                    icon: Iconify(
-                      Ri.home_5_line,
-                      color: Colors.white,
-                    ),
-                    label: 'Home'),
-                BottomNavigationBarItem(
-                    icon: Iconify(
-                      Ri.shopping_bag_fill,
-                      color: Colors.white,
-                    ),
-                    label: 'Reservasi Data'),
-                BottomNavigationBarItem(
-                    icon: Iconify(
-                      Ri.menu_2_fill,
-                      color: Colors.white,
-                    ),
-                    label: 'History'),
-              ]));
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+            type: BottomNavigationBarType.fixed,
+            selectedItemColor: Colors.white,
+            backgroundColor: Colors.black,
+            showSelectedLabels: true,
+            showUnselectedLabels: false,
+            currentIndex: controller.tabIndex,
+            onTap: (index) {
+              controller.changeTabIndex(index);
+            },
+            items: [
+              BottomNavigationBarItem(
+                  icon: Iconify(
+                    Ri.home_5_line,
+                    color: Colors.white,
+                  ),
+                  activeIcon: Iconify(
+                    Ri.home_5_fill,
+                    color: Colors.white,
+                  ),
+                  label: 'Home'),
+              BottomNavigationBarItem(
+                  icon: Iconify(
+                    Ri.shopping_bag_line,
+                    color: Colors.white,
+                  ),
+                  activeIcon: Iconify(
+                    Ri.shopping_bag_fill,
+                    color: Colors.white,
+                  ),
+                  label: 'Reservasi'),
+              BottomNavigationBarItem(
+                  icon: Iconify(
+                    Ri.menu_2_fill,
+                    color: Colors.white,
+                  ),
+                  label: 'History'),
+              BottomNavigationBarItem(
+                  icon: Iconify(
+                    Ri.user_2_fill,
+                    color: Colors.white,
+                  ),
+                  label: 'Admin'),
+            ]),
+      );
     });
   }
 }
